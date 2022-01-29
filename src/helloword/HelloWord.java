6eb5d6ee35d94aa376346594aed2391433e67adf
MagdaNaldo <97902668+MagdaNaldo@users.noreply.github.com>
2022-01-29 13:51:04 -0500

    add both int

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -18,8 +18,10 @@ public class HelloWord {
     public static void main(String[] args) {
         
         int a = 2;
+        int b = 5;
+        int c = a + b;
         
-        System.out.println(a);
+        System.out.println(c);
        
     }
 
